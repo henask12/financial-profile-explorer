5f310f34b18fcd0f2532de898f0ec546be5eee1d
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-09 01:33:55 +0300

    made some changes to slice file

--- src/redux/catagory/catagoriesSlice.js
+++ src/redux/catagory/catagoriesSlice.js
@@ -1,33 +1,62 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import apiService from './apiService'; // Import your API service
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import companiesData from '../../constant/companiesData.json';
 
-export const fetchCategories = createAsyncThunk('categories/fetchCategories', async () => {
-  const categories = await apiService.fetchCategories();
-  return categories;
+const API_KEY = '11d7e6b8232bd10c3d08da305786902b';
+const API_BASE_URL = 'https://financialmodelingprep.com/api/v3';
+
+const initialState = {
+  loading: false,
+  performingAction: false,
+  error: false,
+  success: false,
+  message: '',
+  companies: [],
+};
+
+export const fetchCategories = createAsyncThunk('catagories/fetchCategories', async () => {
+  try {
+    const symbolString = companiesData.symbols.join(',');
+    const companies = await axios.get(
+      `${API_BASE_URL}/profile/${symbolString}?apikey=${API_KEY}`,
+    );
+    return companies.data;
+  } catch (error) {
+    return error.companies.data;
+  }
 });
+
+// Slice
 
 const categoriesSlice = createSlice({
   name: 'categories',
-  initialState: {
-    data: [],
-    status: 'idle', // idle | loading | succeeded | failed
-    error: null,
+  initialState,
+  reducers: {
+    RESET_VALUE(state) {
+      state.error = false;
+      state.success = false;
+      state.performingAction = false;
+    },
   },
-  reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchCategories.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(fetchCategories.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.data = action.payload;
-      })
-      .addCase(fetchCategories.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      });
+    builder.addCase(fetchCategories.pending, (state) => {
+      state.loading = true;
+      state.performingAction = true;
+    });
+    builder.addCase(fetchCategories.fulfilled, (state, action) => {
+      state.loading = false;
+      state.performingAction = false;
+      state.companies = action.payload;
+    });
+    builder.addCase(fetchCategories.rejected, (state, action) => {
+      state.loading = false;
+      state.error = true;
+      state.performingAction = false;
+      state.message = action.payload?.errors[0];
+      state.companies = [];
+    });
   },
 });
 
+export const { RESET_VALUE } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
