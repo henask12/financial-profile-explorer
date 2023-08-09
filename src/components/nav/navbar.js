b97e3c9791f95cb063e141350d89a620a2ce7ad3
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-09 13:20:52 +0300

    navigation done on the navbar

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { FaAngleLeft, FaMicrophone, FaCog } from 'react-icons/fa';
+import { FaAngleLeft, FaCog, FaMicrophone } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
-const Header = () => {
+const HeaderWithBackButton = () => {
   const navigate = useNavigate();
 
-  const handleBackClick = () => {
+  const handleBack = () => {
     navigate(-1); // Navigate back
   };
 
   return (
     <nav className="bg-dark-pink">
       <div className="container mx-auto flex items-center justify-between py-4">
-        <button type="button" className="text-body-primary" onClick={handleBackClick}>
+        <button type="button" className="text-body-primary" onClick={handleBack}>
           <FaAngleLeft className="text-body-primary" size={26} color="white" />
         </button>
         <h3 className="mb-0 text-white">finance</h3>
@@ -25,4 +25,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default HeaderWithBackButton;
