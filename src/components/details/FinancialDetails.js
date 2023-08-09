b47f48b25b396aa329e803d1716635cf88b6fe0e
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-09 21:21:14 +0300

    details page updated

--- src/components/details/FinancialDetails.js
+++ src/components/details/FinancialDetails.js
@@ -1,20 +1,61 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { FaArrowRight } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
 
 const CompanyDetails = () => {
   const location = useLocation();
   const { company } = location.state;
 
-  console.log(company);
   return (
-    <div className="company-details">
-      <h2>{company?.companyName}</h2>
-      <p>
-        Price:
-        {company?.price}
-      </p>
-      {/* Render other details as needed */}
+    <div className="container mx-auto px-8 sm:px-8 lg:px-8">
+      <div className="hero flex justify-center items-center p-3">
+        <img
+          src={company.image}
+          alt={company.companyName}
+          className="logo w-50"
+        />
+        <div className="w-100 text-center ml-6 text-xl">
+          <h1 className="font-bold text-3xl">{company.symbol}</h1>
+          <span>{company.price}</span>
+        </div>
+      </div>
+      <h5 className="mt-2 pb-1 text-center font-semibold text-3xl">
+        {company.companyName}
+  &nbsp;info
+      </h5>
+
+      <div className="company__data">
+        <div className="container">
+          {Object.keys(company)
+            .filter(
+              (value) => value !== 'image'
+          && value !== 'defaultImage'
+          && value !== 'isEtf'
+          && value !== 'isActivelyTrading'
+          && value !== 'isAdr'
+          && value !== 'isFund'
+          && value !== 'description',
+            )
+            .map((value, index) => (
+              <div
+                key={value}
+                className={`border-t py-3 flex justify-between items-center ${index % 2 === 0 ? 'bg-rose-500' : 'bg-pink-700'}`}
+              >
+                <span>{value}</span>
+                <div className="flex items-center">
+                  <span className="p-3">{company[value]}</span>
+                  <FaArrowRight
+                    className="FaArrowRight"
+                    size={22}
+                    color="white"
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+
     </div>
   );
 };
