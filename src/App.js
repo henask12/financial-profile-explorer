e808ac20d50d477f86e8d3e70648b06afe1775f8
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-08-09 13:21:37 +0300

    minor change on the routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import {
-  BrowserRouter,
+  BrowserRouter, Route, Routes,
 } from 'react-router-dom';
 import './App.css';
-import Header from './components/nav/navbar';
 import CompaniesList from './components/list/FinancialList';
+import HeaderWithBackButton from './components/nav/navbar';
+import CompanyDetails from './components/details/FinancialDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="bg-pink-700">
-        <Header />
-        <CompaniesList />
+        <HeaderWithBackButton />
+        <Routes>
+          <Route path="/" element={<CompaniesList />} />
+          <Route path="/company/:symbol" element={<CompanyDetails />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
